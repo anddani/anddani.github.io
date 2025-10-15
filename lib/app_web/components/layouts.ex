859863deb35fa9e59136a2026fe8b3c6b8c1e660
Andre Danielsson <andredanielsson93@gmail.com>
2025-10-15 14:08:28 -0700

    Add fonts

--- lib/app_web/components/layouts.ex
+++ lib/app_web/components/layouts.ex
@@ -48,7 +48,7 @@ defmodule AppWeb.Layouts do
       
     <!-- Header -->
       <header class="w-full px-4 py-8 sm:px-6 lg:px-8 bg-transparent border-b-gray-600 border-b-2">
-        <h1>Welcome!</h1>
+        <h1 class="text-[42px]">Welcome!</h1>
       </header>
 
       <div class="flex flex-row min-h-0">
