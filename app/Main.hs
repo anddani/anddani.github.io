1e37d26dfba37428e603312767b3772474e05eb2
Andre Danielsson <andredanielsson93@gmail.com>
2026-02-28 10:01:59 -0800

    Add base files

---
+++ app/Main.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Hakyll
+
+main :: IO ()
+main = hakyll $ do
+  match "images/*" $ do
+    route   idRoute
+    compile copyFileCompiler
+
+  match "css/style.css" $ do
+    route   idRoute
+    compile copyFileCompiler
+
+  match "templates/*" $ compile templateBodyCompiler
+
+  match "index.md" $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= relativizeUrls
---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Personal website built with Hakyll and Tailwind";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        hpkgs = pkgs.haskellPackages;
+
+        # The Hakyll site generator binary
+        generator = hpkgs.developPackage { root = ./.; };
+
+        # The fully built static site
+        website = pkgs.stdenv.mkDerivation {
+          name = "website";
+          src = ./.;
+          nativeBuildInputs = [ generator pkgs.tailwindcss ];
+          buildPhase = ''
+            tailwindcss -i css/input.css -o css/style.css --minify
+            site build
+          '';
+          installPhase = ''
+            cp -r _site $out
+          '';
+        };
+      in
+      {
+        packages = {
+          default = website;
+          inherit generator;
+        };
+
+        devShells.default = hpkgs.developPackage {
+          root = ./.;
+          returnShellEnv = true;
+          modifier = drv:
+            pkgs.haskell.lib.addBuildTools drv [
+              hpkgs.cabal-install
+              hpkgs.haskell-language-server
+              pkgs.tailwindcss
+            ];
+        };
+      });
+}
